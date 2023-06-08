8c9110d30a60759c6b072595142529032ce3660c
Manua Voltaire <manua.voltaire@gmail.com>
2023-06-08 21:52:30 +0200

    images work, UI needs fixes

--- Wikimage.js
+++ Wikimage.js
@@ -37,9 +37,14 @@ const styles = StyleSheet.create({
 export function WikImage({ navigation }) {
 	
 	// const [searchkey, setsearchkey] = useState("");
-
+	const [imgUri, setimgUri] = useState("");
+	const [imajis, setimajis] = useState("");
+	var imcount = -1;
+	
+	const [text, onChangeText] = React.useState('');
+	
 	const wkibrows = async () => {
-		const api = `https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch=${text}`;
+		const api = `https://en.wikipedia.org/w/api.php?action=query&redirects=1&format=json&prop=images&imlimit=max&origin=*&titles=${text}`;
 		try {
 			const res = await fetch(api, {
 				method: 'GET', headers: {
@@ -47,8 +52,40 @@ export function WikImage({ navigation }) {
 				}
 			});
 			const json = await res.json();
+			
+			var imajs = [];
+			var pages = json.query.pages;
+			for (var page in pages) {
+				for (var img of pages[page].images) {
+					// console.log(img.title);
+					imajs.push(encodeURI(img.title));
+				}
+			}
+			
+			setimgUri(`https://commons.wikimedia.org/wiki/${imajs[1]}`);
+			// setimgUri(`https://upload.wikimedia.org/wikipedia/commons/b/b5/1dayoldkitten.JPG`);
+			// setimgUri(`https://i.redd.it/i5aksclir04b1.jpg`);
+			const imgsrc = `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=imageinfo&iiprop=url&origin=*&titles=${imajs[0]}`;
+			const imgres = await fetch(imgsrc, {
+				method: 'GET', headers: {
+					"Accept": "application/json"
+				}
+			});
+			const imgjson = await imgres.json();
+			var imajis = "";
+			var ipages = imgjson.query.pages;
+			for (var page in ipages) {
+				for (var img of ipages[page].imageinfo) {
+					// console.log(img.title);
+					setimajis(encodeURI(img.url));
+				}
+			}
 			// setsearchkey(json.title);
-			console.log(json);
+			// console.log(json);
+			console.log(imgjson);
+			console.log(imajis);
+			// console.log(imgUri);
+			// console.log(imajs.length);
 		}
 		catch (error) {
 			console.error(error);
@@ -57,8 +94,6 @@ export function WikImage({ navigation }) {
 		//   setLoading(false);
 	}
 	
-	const [text, onChangeText] = React.useState('');
-
 	return (
 		<View style={styles.container}>
 			<LinearGradient
@@ -66,12 +101,7 @@ export function WikImage({ navigation }) {
 				colors={['rgba(250,100,150,0.7)', '#2d8794']}
 				style={styles.background}>
 					<Text style={{ fontSize: 25, fontWeight: 'bold' }}>Browse images from wikipedia{'\n'}</Text>
-	
-					{/* </View> */}
-					{/* <Image source={{ uri: imgUri }}
-        style={{ width: '100%', height: '100%', resizeMode: 'contain', }} /> */}
-					<Text>{'\n\n'}Displays images from wikipedia articles.{'\n\n'}
-					</Text>
+					<Text>{'\n\n'}Display images from wikipedia articles.{'\n\n'}</Text>
 					<Pressable
 						onPress={() => navigation.popToTop()}
 						style={({ pressed }) => [
@@ -106,8 +136,9 @@ export function WikImage({ navigation }) {
 					<Text style={styles.text}>
 						Browse Wikipedia
 					</Text>
-
 				</Pressable>
+						<Image source={{ uri: imajis }}
+							style={{ width: '100%', height: '100%', resizeMode: 'contain'}} />
 			</LinearGradient>
 		</View>
 
